5f0839cc2a886fda3f5cdaf5d6aec693940d8f39
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-07-06 19:48:45 +0200

    Include images on cards

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import MovieCard from "./MovieCard";
+import img1 from "./images/example-1.jpg";
+import img2 from "./images/example-2.jpg";
+import img3 from "./images/example-3.jpg";
 
 export default function App() {
   return (
@@ -7,6 +10,7 @@ export default function App() {
       <div className="row">
         <div className="col">
           <MovieCard
+            image={img1}
             title="Litter-ally Famous"
             synopsis="When Tommy (Jim Carrey), a curious and mischievous cat, discovers a human toilet, his life takes an unexpected turn. With the help of his devoted pet-parent, Charlie (Tina Fey), Tommy learns how to use the toilet and soon becomes a worldwide celebrity. But as Tommy's fame grows, so does the attention of an evil scientist bent on using Tommy's toilet-training skills to take over the world. With the fate of the world in their hands, Charlie and Tommy must find a way to thwart the scientist's evil plan. Can the unlikely duo save the world from destruction?"
             idea="A cat learns to use a human toilet and becomes world famous"
@@ -14,6 +18,7 @@ export default function App() {
         </div>
         <div className="col">
           <MovieCard
+            image={img2}
             title="Codes to Contentment"
             synopsis="David (Eddie Redmayne), a language teacher, longs for a new challenge and feels stuck in a never-ending rut of monotony. When an opportunity opens up to move to the Netherlands and teach English, David jumps on it. But when his students don't show up to his classes, David takes up coding to challenge himself and find purpose. With the help of his eccentric new codemates and a demanding mentor (Helena Bonham Carter), David slowly finds his place in the world and discovers a newfound passion - coding. This inspiring and life-affirming story follows David as he learns to find purpose and appreciation in life's unexpected detours."
             idea="A language teacher moves to the Netherlands and learns to code"
@@ -21,6 +26,7 @@ export default function App() {
         </div>
         <div className="col">
           <MovieCard
+            image={img3}
             title="Veggie Vengeance"
             synopsis="When green-thumb Samantha (Emily Blunt) discovers a passion for houseplants, she dives into an obsession that quickly takes over her life and ruins her relationship with her husband, Lucas (Hugh Jackman). With every new plant purchase, Samantha's house is gradually transformed into an otherworldly jungle. But when the plants begin to act strangely, strange accidents start happening and Samantha's life is put in danger. As the plants close in on her, Samantha discovers the truth - the plants are not only attempting to take over her house, but her life too! Will she survive the onslaught of the home-invading plants, or succumb to their vegetative wrath?"
             idea="A woman becomes obsessed with houseplants and eventually her house becomes such a jungle that the plants turn on her and she almost dies"
--- src/MovieCard.js
+++ src/MovieCard.js
@@ -1,6 +1,7 @@
-function MovieCard({ title, synopsis, idea }) {
+function MovieCard({ image, title, synopsis, idea }) {
   return (
     <div className="card">
+      <img src={image} alt="Movie poster" className="card-img-top" />
       <div className="card-body">
         <h4 className="text-center">{title}</h4>
         <p className="card-text">{synopsis}</p>
