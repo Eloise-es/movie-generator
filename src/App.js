27dac24423bdfe945d3f195ecd66cee3922ce7f9
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-07-06 18:57:03 +0200

    Built basic outline

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import MovieCard from "./MovieCard";
+
+export default function App() {
+  return (
+    <div>
+      <div>Movies made with Movie Generator</div>
+      <MovieCard />
+      <MovieCard />
+      <MovieCard />
+    </div>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
+import App from "./App";
+
+const el = document.getElementById("root");
+const root = ReactDOM.createRoot(el);
+
+root.render(<App />);
