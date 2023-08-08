20bccc6d2e66c7579649525e23c51f1428bd4511
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-08-08 21:47:57 +0200

    Set up routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from "./App";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import Root from "./routes/root";
+import ErrorPage from "./Error";
+import Display from "./Display";
 
-const el = document.getElementById("root");
-const root = ReactDOM.createRoot(el);
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: "/movies",
+				element: <Display />,
+			},
+		],
+	},
+]);
 
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
