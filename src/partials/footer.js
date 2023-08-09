400e02807e7693a9b22edb333f0ea842b3eb78a4
Eloise-es <101559841+Eloise-es@users.noreply.github.com>
2023-08-09 16:27:58 +0200

    Corrected links in footer

--- src/partials/footer.js
+++ src/partials/footer.js
@@ -1,3 +1,4 @@
+import { LinkContainer } from "react-router-bootstrap";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -7,10 +8,14 @@ export default function Footer() {
 		<Navbar fixed="bottom" bg="dark" data-bs-theme="dark">
 			<Container>
 				<Nav className="m-auto">
-					<Navbar.Brand href="#home">
-						Artificial Intelligence Movie Database
-					</Navbar.Brand>
-					<Nav.Link href="https://www.eloise.es">by Eloise Salmon</Nav.Link>
+					<LinkContainer to={"/"}>
+						<Navbar.Brand href="#home">
+							Artificial Intelligence Movie Database
+						</Navbar.Brand>
+					</LinkContainer>
+					<Nav.Link href="https://www.eloise.es" target="_blank">
+						by Eloise Salmon
+					</Nav.Link>
 				</Nav>
 			</Container>
 		</Navbar>
